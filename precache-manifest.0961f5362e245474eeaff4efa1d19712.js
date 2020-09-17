806e1c73cbfea3ff950f8de6da3a67e5a6da2458
mark-nikerin <mrnikerin@gmail.com>
2020-09-18 02:21:16 +0400

    Updates

---
+++ precache-manifest.0961f5362e245474eeaff4efa1d19712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f26683065df11260b6e8f0c42ab424",
+    "url": "/vezdekod-web-task-2/index.html"
+  },
+  {
+    "revision": "150fd1796b343fe7c113",
+    "url": "/vezdekod-web-task-2/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "88f595cb6d3aeddbb97b",
+    "url": "/vezdekod-web-task-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "150fd1796b343fe7c113",
+    "url": "/vezdekod-web-task-2/static/js/2.6439baeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vezdekod-web-task-2/static/js/2.6439baeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f595cb6d3aeddbb97b",
+    "url": "/vezdekod-web-task-2/static/js/main.de93f778.chunk.js"
+  },
+  {
+    "revision": "01212348e76d261f8459",
+    "url": "/vezdekod-web-task-2/static/js/runtime-main.fa09f02f.js"
+  }
+]);
